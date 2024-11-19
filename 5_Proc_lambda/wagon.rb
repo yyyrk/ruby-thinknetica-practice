d2a1ad04fa626650091f5243c60d02d143a2c50d
Andrew Yurik <78261534+yyyrk@users.noreply.github.com>
2024-11-19 06:51:35 +0300

    Update wagon.rb

--- 5_Proc_lambda/wagon.rb
+++ 5_Proc_lambda/wagon.rb
@@ -3,9 +3,31 @@ require_relative 'manufactures'
 class Wagon
   include Manufactures
 
-  attr_reader :type
+  attr_reader :number, :type, :amount, :busy, :unbusy
 
-  def initialize(type)
-    @type = type
+  def initialize(number, type, amount)
+    @number    = number
+    @type      = type
+    @amount    = amount
+    validate!
+    @busy = 0
+    @unbusy = amount
   end
+
+  def fill(qtty)
+    @busy = busy + qtty
+    @busy = busy - qtty unless valid?
+    @unbusy = amount - busy
+  end
+
+  def valid?
+    validate!
+  rescue
+    false
+  end
+
+  protected
+
+  attr_writer :number, :type, :speed, :route, :wagons
+
 end
