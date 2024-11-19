30f88cbf40ad091f5eda3e430fe5b4d353233cdc
Andrew Yurik <78261534+yyyrk@users.noreply.github.com>
2024-11-19 06:49:51 +0300

    Update passenger_train.rb

--- 5_Proc_lambda/passenger_train.rb
+++ 5_Proc_lambda/passenger_train.rb
@@ -4,7 +4,11 @@ class PassengerTrain < Train
   attr_reader :type
 
   def initialize(number)
-    super
+    super(number)
     @type = :passenger
+  end
+
+  def add_wagon(wagon)
+    super if wagon.type == :passenger
   end
 end
