1ab9e04baeb2a9d14c0e9b8469ee92ff1045616f
Andrew Yurik <andrei.iurik@gmail.com>
2024-11-28 13:51:04 +0300

    Add exception handling for station, train, wagon creation, route assignment, and other menu actions to prevent crashes and ensure smoother error handling

--- 6_Metaprogramming/manage.rb
+++ 6_Metaprogramming/manage.rb
@@ -1,6 +1,11 @@
 require_relative 'train'
 require_relative 'accessors'
 require_relative 'validations'
+\
+
+
+
+
 
 class Manager
   attr_reader :wagons, :trains, :stations, :routes
@@ -81,10 +86,17 @@ ___________|||______________________________|______________/
   def create_station
     print 'Введите название вашей станции: '
     name = gets.chomp.capitalize
-    station = Station.new(name)
-    @stations << station
-    puts "Станция #{name} создана."
+    begin
+      station = Station.new(name)
+
+      station.validate!
+      @stations << station
+      puts "Станция #{name} создана."
+    rescue StandardError => e
+      puts "Ошибка при создании станции: #{e.message}. Станция не создана."
+    end
   end
+
 
   # 2. Создать поезд
   def create_train
@@ -92,24 +104,29 @@ ___________|||______________________________|______________/
     number = gets.chomp
 
     print 'Выберите тип поезда:
-    1. Пассажирский
-    2. Грузовой
-    Номер типа поезда: '
+  1. Пассажирский
+  2. Грузовой
+  Номер типа поезда: '
     option = gets.strip
 
-    case option
-    when '1'
-      train = PassengerTrain.new(number)
-    when '2'
-      train = CargoTrain.new(number)
-    else
-      puts 'Неверный тип поезда.'
-      return
-    end
+    begin
+      case option
+      when '1'
+        train = PassengerTrain.new(number)
+      when '2'
+        train = CargoTrain.new(number)
+      else
+        raise 'Неверный тип поезда.'
+      end
 
-    @trains << train
-    puts "Поезд #{number} (#{train.type}) создан."
+      train.validate!
+      @trains << train
+      puts "Поезд #{number} (#{train.type}) создан."
+    rescue StandardError => e
+      puts "Ошибка при создании поезда: #{e.message}. Поезд не создан."
+    end
   end
+
 
   # 3. Создать вагон
   def create_wagon
@@ -118,144 +135,177 @@ ___________|||______________________________|______________/
 
     option = gets.strip
 
-    case option
-    when '1'
-      print 'Введите номер вагона: '
-      number = gets.strip
-      print 'Введите количество мест: '
-      places = gets.strip.to_i
-      wagon = PassengerWagon.new(number, places)
-      @wagons << wagon
-      puts "Пассажирский вагон #{wagon.number} с #{places} местами создан."
-    when '2'
-      print 'Введите номер грузового вагона: '
-      number = gets.strip
-      print 'Введите вместимость (объем): '
-      capacity = gets.strip.to_i
-      wagon = CargoWagon.new(number, capacity)
-      @wagons << wagon
-      puts "Грузовой вагон #{wagon.number} с вместимостью #{capacity} создан."
-    else
-      puts 'Неверный выбор.'
+    begin
+      case option
+      when '1'
+        print 'Введите номер вагона: '
+        number = gets.strip
+        print 'Введите количество мест: '
+        places = gets.strip.to_i
+        raise 'Количество мест должно быть положительным числом!' if places <= 0
+        wagon = PassengerWagon.new(number, places)
+        @wagons << wagon
+        puts "Пассажирский вагон #{wagon.number} с #{places} местами создан."
+      when '2'
+        print 'Введите номер грузового вагона: '
+        number = gets.strip
+        print 'Введите вместимость (объем): '
+        capacity = gets.strip.to_i
+        raise 'Вместимость должна быть положительным числом!' if capacity <= 0
+        wagon = CargoWagon.new(number, capacity)
+        @wagons << wagon
+        puts "Грузовой вагон #{wagon.number} с вместимостью #{capacity} создан."
+      else
+        raise 'Неверный выбор типа вагона.'  # Если выбран неверный тип вагона
+      end
+    rescue StandardError => e
+      puts "Ошибка при создании вагона: #{e.message}. Вагон не создан."
     end
   end
 
+
   # 4. Создать маршрут
   def create_route
-    print 'Введите номер начальной станции в маршруте: '
-    list_stations
-    first_station = gets.strip.to_i - 1
+    begin
+      print 'Введите номер начальной станции в маршруте: '
+      list_stations
+      first_station = gets.strip.to_i - 1
 
-    print 'Введите номер конечной станции в маршруте: '
-    list_stations
-    last_station = gets.strip.to_i - 1
+      print 'Введите номер конечной станции в маршруте: '
+      list_stations
+      last_station = gets.strip.to_i - 1
 
-    if first_station == last_station
-      puts 'Начальная и конечная станции не могут быть одинаковыми.'
-    else
-      @routes << Route.new(@stations[first_station], @stations[last_station])
+      if first_station == last_station
+        raise 'Начальная и конечная станции не могут быть одинаковыми.'
+      end
+
+      route = Route.new(@stations[first_station], @stations[last_station])
+      @routes << route
       puts "Маршрут создан: #{@stations[first_station].name} - #{@stations[last_station].name}"
+    rescue StandardError => e
+      puts "Ошибка при создании маршрута: #{e.message}. Маршрут не создан."
     end
   end
 
   # 5. Добавить/удалить станцию из маршрута
   def modify_route
-    print 'Введите номер маршрута для редактирования: '
-    list_routes
-    route_number = gets.strip.to_i - 1
+    begin
+      print 'Введите номер маршрута для редактирования: '
+      list_routes
+      route_number = gets.strip.to_i - 1
 
-    route = @routes[route_number]
-    puts '1. Добавить станцию'
-    puts '2. Удалить станцию'
-    option = gets.strip
+      route = @routes[route_number]
+      puts '1. Добавить станцию'
+      puts '2. Удалить станцию'
+      option = gets.strip
 
-    print 'Введите номер станции для добавления/удаления: '
-    list_stations
-    station_number = gets.strip.to_i - 1
+      print 'Введите номер станции для добавления/удаления: '
+      list_stations
+      station_number = gets.strip.to_i - 1
 
-    station = @stations[station_number]
-    case option
-    when '1'
-      route.add_station(station)
-      puts "Станция #{station.name} добавлена в маршрут."
-    when '2'
-      route.delete_station(station)
-      puts "Станция #{station.name} удалена из маршрута."
-    else
-      puts 'Неверный выбор.'
+      station = @stations[station_number]
+      case option
+      when '1'
+        route.add_station(station)
+        puts "Станция #{station.name} добавлена в маршрут."
+      when '2'
+        route.delete_station(station)
+        puts "Станция #{station.name} удалена из маршрута."
+      else
+        raise 'Неверный выбор. Выберите 1 или 2.'
+      end
+    rescue StandardError => e
+      puts "Ошибка при редактировании маршрута: #{e.message}. Изменения не применены."
     end
   end
 
+
   # 6. Назначить поезду маршрут
   def assign_route_to_train
-    print 'Введите номер маршрута: '
-    list_routes
-    route_number = gets.strip.to_i - 1
+    begin
+      print 'Введите номер маршрута: '
+      list_routes
+      route_number = gets.strip.to_i - 1
 
-    print 'Введите номер поезда: '
-    list_trains
-    train_number = gets.strip.to_i - 1
+      print 'Введите номер поезда: '
+      list_trains
+      train_number = gets.strip.to_i - 1
 
-    route = @routes[route_number]
-    train = @trains[train_number]
+      route = @routes[route_number]
+      train = @trains[train_number]
 
-    train.get_route(route)
-    puts "Поезду #{train.number} назначен маршрут #{route.first_station.name} - #{route.last_station.name}"
+      train.get_route(route)
+      puts "Поезду #{train.number} назначен маршрут #{route.first_station.name} - #{route.last_station.name}"
+    rescue IndexError => e
+      puts "Ошибка: Неверный номер маршрута или поезда. Попробуйте снова."
+    rescue StandardError => e
+      puts "Ошибка при назначении маршрута поезду: #{e.message}. Операция не выполнена."
+    end
   end
+
 
   # 7. Добавить/отцепить вагон
   def manage_wagons
-    print 'Выберите действие:
+    begin
+      print 'Выберите действие:
     1. Присоединить вагон
     2. Отцепить вагон'
-    option = gets.strip
+      option = gets.strip
 
-    print 'Выберите вагон: '
-    list_wagons
-    wagon_number = gets.strip.to_i - 1
+      print 'Выберите вагон: '
+      list_wagons
+      wagon_number = gets.strip.to_i - 1
 
-    print 'Выберите поезд: '
-    list_trains
-    train_number = gets.strip.to_i - 1
+      print 'Выберите поезд: '
+      list_trains
+      train_number = gets.strip.to_i - 1
 
-    wagon = @wagons[wagon_number]
-    train = @trains[train_number]
+      wagon = @wagons[wagon_number]
+      train = @trains[train_number]
 
-    case option
-    when '1'
-      train.connect_wagon(wagon)
-      puts "Вагон #{wagon.number} присоединен к поезду #{train.number}."
-    when '2'
-      train.delete_wagon(wagon)
-      puts "Вагон #{wagon.number} отцеплен от поезда #{train.number}."
-    else
-      puts 'Неверный выбор.'
+      case option
+      when '1'
+        train.connect_wagon(wagon)
+        puts "Вагон #{wagon.number} присоединен к поезду #{train.number}."
+      when '2'
+        train.delete_wagon(wagon)
+        puts "Вагон #{wagon.number} отцеплен от поезда #{train.number}."
+      else
+        raise 'Неверный выбор. Выберите 1 или 2.'
+      end
+    rescue StandardError => e
+      puts "Ошибка при управлении вагонами: #{e.message}. Операция не выполнена."
     end
   end
 
   # 8. Отправить поезд по маршруту
   def send_train_on_route
-    print '1. Переместить поезд вперед'
-    print '2. Переместить поезд назад'
-    option = gets.strip
+    begin
+      print '1. Переместить поезд вперед'
+      print '2. Переместить поезд назад'
+      option = gets.strip
 
-    print 'Выберите поезд: '
-    list_trains
-    train_number = gets.strip.to_i - 1
+      print 'Выберите поезд: '
+      list_trains
+      train_number = gets.strip.to_i - 1
 
-    train = @trains[train_number]
+      train = @trains[train_number]
 
-    case option
-    when '1'
-      train.forward
-      puts "Поезд #{train.number} перемещен вперед."
-    when '2'
-      train.back
-      puts "Поезд #{train.number} перемещен назад."
-    else
-      puts 'Неверный выбор.'
+      case option
+      when '1'
+        train.forward
+        puts "Поезд #{train.number} перемещен вперед."
+      when '2'
+        train.back
+        puts "Поезд #{train.number} перемещен назад."
+      else
+        raise 'Неверный выбор. Выберите 1 или 2.'
+      end
+    rescue StandardError => e
+      puts "Ошибка при перемещении поезда: #{e.message}. Операция не выполнена."
     end
   end
+
 
   # 9. Вывести список вагонов у поезда
   def show_wagons
@@ -278,24 +328,28 @@ ___________|||______________________________|______________/
 
   # 11. Занять место или объем в вагоне
   def occupy_wagon
-    print 'Выберите вагон: '
-    list_wagons
-    wagon_number = gets.strip.to_i - 1
-    wagon = @wagons[wagon_number]
+    begin
+      print 'Выберите вагон: '
+      list_wagons
+      wagon_number = gets.strip.to_i - 1
+      wagon = @wagons[wagon_number]
 
-    case wagon
-    when PassengerWagon
-      print 'Введите количество мест для бронирования: '
-      places = gets.strip.to_i
-      wagon.take_places(places)
-      puts "#{places} мест забронировано в вагоне #{wagon.number}."
-    when CargoWagon
-      print 'Введите объем для бронирования: '
-      volume = gets.strip.to_i
-      wagon.take_capacity(volume)
-      puts "#{volume} объема забронировано в вагоне #{wagon.number}."
-    else
-      puts 'Неизвестный тип вагона.'
+      case wagon
+      when PassengerWagon
+        print 'Введите количество мест для бронирования: '
+        places = gets.strip.to_i
+        wagon.take_places(places)
+        puts "#{places} мест забронировано в вагоне #{wagon.number}."
+      when CargoWagon
+        print 'Введите объем для бронирования: '
+        volume = gets.strip.to_i
+        wagon.take_capacity(volume)
+        puts "#{volume} объема забронировано в вагоне #{wagon.number}."
+      else
+        raise 'Неизвестный тип вагона.'
+      end
+    rescue StandardError => e
+      puts "Ошибка при бронировании: #{e.message}. Операция не выполнена."
     end
   end
 
